bbdbfb71dbd709a28e928cb11e3eaf51ae04b2e1
degenerat3 <james.maskelony@gmail.com>
2019-02-28 12:07:58 -0500

    required endpoints/functions stubbed out

---
+++ server/app.py
@@ -0,0 +1,91 @@
+"""
+Process POST requests from clients, log data to files so it can be sent with filebeat
+@author: degenerat3
+"""
+
+import datetime
+import os
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+def log_fw(ip, rules):
+    """
+    Take in string, which will be all iptables output, split it into one rule per line, log in form:
+    IP      FIREWALL        <RULE>
+    so each line of log file has one rule
+    @param ip: The source IP address, will be used as identifer
+    @param rules: the output of all the iptables commands (including newlines)
+    @return: None    
+    """
+    for rule in rules:
+        print(rule)
+    return
+
+
+def log_hosts(ip, hosts):
+    """
+    Take in string, which will be the /etc/hosts file, split it into one host per line, log in form:
+    IP      HOSTS        <host_ip>       <hostname>
+    so each line of log file has "host" entry
+    @param ip: The source IP address, will be used as identifer
+    @param hosts: the output of "cat /etc/hosts" (including newlines)
+    @return: None    
+    """
+    for host in hosts:
+        print(host)
+    return
+
+
+def log_routes(ip, routes):
+    """
+    Take in string, which will be all ip routes output, split it into one route per line, log in form:
+    IP      ROUTE        <Route Data>
+    so each line of log file has one route
+    @param ip: The source IP address, will be used as identifer
+    @param routes: the output of the "ip route" (including newlines)
+    @return: None    
+    """
+    for route in routes:
+        print(route)
+    return
+
+
+def log_arp(ip, arp):
+    """
+    Take in string, which will be arp table, split it into one entry per line, log in form:
+    IP      ARP        <entry>
+    so each line of log file has one arp entry
+    @param ip: The source IP address, will be used as identifer
+    @param arp: output of "arp -a" (including newlines)
+    @return: None    
+    """
+    for entry in arp:
+        print(entry)
+    return
+
+
+@app.route('/campfire', methods=['POST'])
+def process_campfire():
+    content = request.json
+    ip = content['ip']
+    fwall = content['rules']
+    hosts = content['etchosts']
+    routes = content['routes']
+    arp = content['arp']
+
+    log_fw(ip, fwall)
+    log_hosts(ip, hosts)
+    log_routes(ip, routes)
+    log_arp(ip, arp)
+
+    return
+
+
+@app.route('/scavpro', methods=['POST'])
+def process_scavpro():
+    content = request.json
+    return
+
